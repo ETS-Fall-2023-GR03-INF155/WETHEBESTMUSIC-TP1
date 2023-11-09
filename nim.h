c209919733de9476d41fdf1f73fc7c0fa9fbbae7
Noah Tremblay <noah.tremblay.1@ens.etsmtl.ca>
2023-11-09 12:05:06 -0500

    nim.h update final partie 1

--- nim.h
+++ nim.h
@@ -13,42 +13,29 @@
 /*                       Déclarations des constantes                         */
 /*===========================================================================*/
 
-#define PLATEAU_MAX_COLONNES 20		//Nombre maximal de colonnes sur le plateau
 #define PLATEAU_MAX_PIECES 35		//Nombre maximal de pièces par colonne
 #define TRUE   1
 #define FALSE  0
+
 
 /*===========================================================================*/
 /*                   Prototype des fonctions publiques                       */
 /*===========================================================================*/
 
 //Initialise le plateau de jeu en remplissant les "nb_colonnes" d'un nombre aléatoire de pièces 
-void plateau_init(int plateau[], int nb_colonnes);
+void plateau_init(int[], int);
 
-//Applique des changements à la configuration du plateau de jeu en retirant 
-// "nb_pieces" de la colonne "colonne" du plateau
-int nim_jouer_tour(int plateau[], int nb_colonnes, int colonne,
-                   int nb_pieces);
+//permet de retirer des picèces du pateau si possible (TRUE), renvois FALSE sinon 
+int nim_jouer_tour(int[], int, int, int);
 
-//Supprime la colonne "col_a_supprimer" du plateau
-void plateau_supprimer_colonne(int plateau[], int nb_colonnes,
-                               int col_a_supprimer);
+//utilise la fonction privée plateau supprimer colonne pour suprimer les colones vides
+int plateau_defragmenter(int[], int);
 
-//Fonction qui supprime les colonnes vides du tableau en utilisant la fonction 
-// "plateau_supprimer_colonne"
-int plateau_defragmenter(int plateau[], int nb_colonnes);
+//permet a l'ia de faire un choix aléatoire de colone et de piece
+void nim_choix_ia_aleatoire(int[], int, int*, int*);
 
-//Fonction qui effectue un jeu aléatoire en choisissant au hasard une colonne,
-// puis au hasard le nombre de pièces à jouer de cette colonne
-void nim_choix_ia_aleatoire(const int plateau[], int nb_colonnes,
-                            int* choix_colonne, int* choix_nb_pieces);
-
-//Fonction qui détermine quel doit être le jeu de l'ordinateur. Cette fonction implémente l'algorithme
-// intelligent décrit dans la partie 2 du TP. Le choix de l'ordinateur sera stocké dans les deux références
-// "choix_colonne" et "choix_nb_pieces"
-void nim_choix_ia(const int plateau[], int nb_colonnes, int niveau,
-                  int* choix_colonne, int* choix_nb_pieces);
-
+//choix de l'ia intelligent
+void nim_choix_ia(const int plateau[], int nb_colonnes, int niveau, int* choix_colonne, int* choix_nb_pieces);
 
 /*===========================================================================*/
 #endif // vas avec le #ifndef
